b206f2e9aa6436c232a3ce4c0786b97731d1ea97
Mike Nancett <mike.nancett@gmail.com>
2016-07-01 19:04:43 -0500

    Initial Commit

---
+++ MCMS_REDO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MCMS_REDO
+//
+//  Created by Mike Nancett on 7/1/16.
+//  Copyright © 2016 Mike Nancett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
