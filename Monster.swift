8bf4a211f006336e569b1158202fde8ffe2cd432
Mike Nancett <mike.nancett@gmail.com>
2016-08-03 11:07:01 -0700

    first commit

---
+++ Monster.swift
@@ -0,0 +1,13 @@
+//
+//  Monster.swift
+//  MCMS_REDO
+//
+//  Created by Mike Nancett on 7/1/16.
+//  Copyright © 2016 Mike Nancett. All rights reserved.
+//
+
+import UIKit
+
+class Monster: NSObject {
+
+}
